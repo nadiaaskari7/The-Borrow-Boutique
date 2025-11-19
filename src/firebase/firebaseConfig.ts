9fdafca62fee7729eccec3a742a9b656ebbe5575
nadiaaskari <nask472@aucklanduni.ac.nz>
2025-11-19 18:40:10 +1300

    Initial project setup (Vite + React + TS)

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEb2B1BcjHTYoRdj9t_10t3Q2-6_iGZEo",
+  authDomain: "theborrowboutique-b7006.firebaseapp.com",
+  projectId: "theborrowboutique-b7006",
+  storageBucket: "theborrowboutique-b7006.firebasestorage.app",
+  messagingSenderId: "484019945612",
+  appId: "1:484019945612:web:394fb6311cd60f34277ba9"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
